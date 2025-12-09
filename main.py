c703d1130eb024bcab1dd21147b58c5011b427a2
yassine <yassinebnyahia@gmail.com>
2025-12-09 16:40:49 +0100

    tp ro

---
+++ main.py
@@ -0,0 +1,19 @@
+# main.py - Application principale
+import sys
+from PyQt5.QtWidgets import QApplication
+from gui.main_window import MainWindow
+
+def main():
+    """Point d'entrée de l'application"""
+    app = QApplication(sys.argv)
+    app.setStyle('Fusion')
+    
+    window = MainWindow()
+    window.show()
+    
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
+
+
